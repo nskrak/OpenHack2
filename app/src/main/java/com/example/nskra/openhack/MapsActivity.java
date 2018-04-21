071d8955c8f27af4f5ebc48315c3a420e255a242
nskrak <n.skrak@hotmail.com>
2018-04-21 20:19:41 +0200

    ni

--- app/src/main/java/com/example/nskra/openhack/MapsActivity.java
+++ app/src/main/java/com/example/nskra/openhack/MapsActivity.java
@@ -1,11 +1,18 @@
 package com.example.nskra.openhack;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -15,12 +22,13 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
 import java.net.*;
 import java.util.Map;
 
 
-
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener, GoogleMap.OnMyLocationButtonClickListener,
+        GoogleMap.OnMyLocationClickListener {
 
     private GoogleMap mMap;
     private Map<String, Bikepump> pumps;
@@ -30,8 +38,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     TextView txtLat;
     String lat;
     String provider;
-    protected String latitude,longitude;
-    protected boolean gps_enabled,network_enabled;
+    protected String latitude, longitude;
+    protected boolean gps_enabled, network_enabled;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,7 +49,19 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        //txtLat = (TextView) findViewById(R.id.textview1);
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        try {
 
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+            System.out.println("hewjofinewaoideihopohfdwhjidsfhkhjdsf");
+        } catch (SecurityException e) {
+            System.out.println("Could not get permission");
+        } catch (NullPointerException e2) {
+            System.out.println("NullPointer omg epic fail");
+        } finally {
+            System.out.println("Nothing works!");
+        }
     }
 
 
@@ -57,17 +77,28 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        try{
+        try {
             CykelpumpData cpd = new CykelpumpData(mMap);
             cpd.execute(new URL("https://helsingborg.opendatasoft.com/api/records/1.0/search/?dataset=cykelpumpar"));
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.println(e);
         }
 
-
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+        mMap.setOnMyLocationButtonClickListener(this);
+        mMap.setOnMyLocationClickListener(this);
         // Add a marker in Sydney and move the camera
         LatLng Helsingborg = new LatLng(56.0384836, 12.6966470);
-        mMap.addMarker(new MarkerOptions().position(Helsingborg).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(Helsingborg));
 
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Helsingborg, 14.0f));
@@ -80,6 +111,28 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     }
 
+
+    @Override
+    public void onLocationChanged(Location location) {
+        //txtLat = (TextView) findViewById(R.id.textview1);
+        System.out.println("Latitude:" + location.getLatitude() + ", Longitude:" + location.getLongitude());
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        Log.d("Latitude","disable");
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        Log.d("Latitude","enable");
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        Log.d("Latitude","status");
+    }
+
     private void createCircle(LatLng point){
         CircleOptions circle = new CircleOptions();
         circle.center(point);
@@ -88,5 +141,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         circle.fillColor(0x30ff0000);
         circle.strokeWidth(2);
         mMap.addCircle(circle);
+    }
+
+    @Override
+    public boolean onMyLocationButtonClick() {
+        return false;
+    }
+
+    @Override
+    public void onMyLocationClick(@NonNull Location location) {
+
     }
 }
